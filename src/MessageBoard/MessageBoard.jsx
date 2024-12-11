ab895e9bed8c7eb02507d7964cb4e9bdca2bbf95
Renaud Dubuis <renaud.dubuis@decryptage.net>
2024-12-11 15:19:20 +0100

    Présentation de useEffect

--- src/MessageBoard/MessageBoard.jsx
+++ src/MessageBoard/MessageBoard.jsx
@@ -1,3 +1,6 @@
+
+import { useEffect } from "react";
+import { useUsersApi } from "../logic";
 import Inbox from "./Inbox";
 import Messages from "./Messages";
 import Messaging from "./Messaging";
@@ -9,10 +12,20 @@ import "./_maquette/style.css";
 function MessageBoard() {
 
     // Implémentation provisoire des données. A remplacer par des appels API.
+    /* 
     const conversations = [
         { id: 1, name: "Sunil Rajput", date: "Dec 25", preview: "Test, which is a new approach...", profileImage: "https://ptetutorials.com/images/user-profile.png" },
         // autres conversations
-    ];
+    ]; 
+    */
+
+    const {users, getAllUsers} = useUsersApi();
+    const conversations = users;
+
+    // Appelle de la fonction une seule fois au chargement du composant
+    useEffect(() => {
+        getAllUsers()
+    },[])
 
     const messages = [
         { type: "incoming", text: "Hello there!", timestamp: "11:01 AM | Today", profileImage: "https://ptetutorials.com/images/user-profile.png" },
